dc80bbcf665077251eb15ceef89d74284a3de76f
Fluid.dev <142986025+fluid-developer@users.noreply.github.com>
2024-03-24 18:12:32 +0300

    v2.1 update

--- Launcher.sh
+++ Launcher.sh
@@ -2,22 +2,13 @@
 # Welcome, I don't know what you're seeking for here, it's really finished here.
 # But feel free to edit, just, don't blame if something will break, k?
 
-# base config
-# bash config NOT WORKING! TEST FEATURE!
-KIPHELLO=false
 GAMESDIR=/home/fluid/des-short/.programs/DBLF
 
 # DBL begins
 if [[ $1 == "" ]]
 then
 bash banner c 
-echo "DosBoxLauncher v2"
-
-if [[ $KIPHELLO == "false" ]]
-then
-  echo "Press any key to enter menu"
-  read -p '(You can disable this in Config.)'
-fi
+echo "Launching..."
 MENUKEY=$(cat menu | gum filter)
 
 # Actual code goes here
@@ -27,21 +18,22 @@ bash banner c
 if [[ $MENUKEY == "Config" ]]
 then
   $EDITOR config
+  comp=y
   exit 0
 fi 
 
 if [[ $MENUKEY == "Changelog" ]]
 then
   clear
-  echo "DBL changelog."
-  echo
-  echo "V4 - Full rewrite."
+  cat changelog
+  comp=y
   exit 0
 fi
 
 if [[ $MENUKEY == "Launch (Without mount)" ]]
 then
   bash db
+  comp=y
   exit 0
 fi 
 
@@ -49,13 +41,21 @@ if [[ $MENUKEY == "Launch (Prompt mount folder)" ]]
 then
   MOUNTDIR=$(ls -d -- $GAMESDIR | gum filter)
   bash db $MOUNTDIR
+  comp=y
   exit 0
 fi 
 
 if [[ $MENUKEY == "Exit" ]]
 then
   echo "Exitting..."
+  comp=y
+  exit 0
 fi 
 # END OF DBL
 fi
 
+# Failure report
+if [[ $comp == "" ]]
+then
+  echo "DBL: Wrong argument / Code failure"
+fi 
